42aeff1f32bb1f2421a1e5d02a05c7def6dd3aa5
Argin Simonian <arginsimon@gmail.com>
2020-05-25 16:48:46 -0700

    threejs in a div

--- public/effects.js
+++ public/effects.js
@@ -1,29 +1,34 @@
+var stickyNav, threeD, pigGame;
 /* Sticky Navbar Function
  *******************************/
-window.onscroll = () => { stickyScroll() };
-var navbar = document.getElementById("navigate");
-var sticky = navbar.offsetLeft;
+stickyNav = () => {
+    var navbar, sticky;
+    window.onscroll = () => { stickyScroll() };
+    navbar = document.getElementById("navigate");
+    sticky = navbar.offsetLeft;
 
-function stickyScroll() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
+    function stickyScroll() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
     }
 }
-
+stickyNav();
 /* Introduction Three.js Animation
  ****************************************/
-var threeD = () => {
-    var scene
+threeD = () => {
+    var scene, camera, renderer, container, geometry, material, cube, render;
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerWidth, 0.1, 1000);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerWidth, 0.1, 1000);
 
-    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setClearColor("#e5e5e5");
+    container = document.getElementById('intro');
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    container.appendChild(renderer.domElement);
 
     window.addEventListener('resize', () => {
         renderer.setSize(window.innerWidth, window.innerHeight);
@@ -31,13 +36,13 @@ var threeD = () => {
         camera.updateProjectionMatrix();
     })
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
+    geometry = new THREE.BoxGeometry(1, 1, 1);
+    material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
 
-    var render = () => {
+    render = () => {
         requestAnimationFrame(render);
         renderer.render(scene, camera);
     }
@@ -49,96 +54,98 @@ threeD();
 
 /* Pig Game Code
  **************************************/
+pigGame = () => {
+    var scores, roundScore, activePlayer, gamePlay;
 
-var scores, roundScore, activePlayer, gamePlay;
+    init();
 
-init();
+    var preDice;
 
-var preDice;
+    function nextPlayer() {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
 
-function nextPlayer() {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
 
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    document.querySelector('.dice').style.display = 'none';
-}
+    document.querySelector('.btn-roll').addEventListener('click', () => {
+        if (gamePlay) {
+            // 1. Random number
+            var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('.btn-roll').addEventListener('click', () => {
-    if (gamePlay) {
-        // 1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
-
-        // 2. Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'public/images/dice-' + dice + '.png';
+            // 2. Display the result
+            var diceDOM = document.querySelector('.dice');
+            diceDOM.style.display = 'block';
+            diceDOM.src = 'public/images/dice-' + dice + '.png';
 
 
-        //3. Update the round score IF the rolled number was NOT a 1
-        if (dice === 6 && preDice === 6) {
-            scores[activePlayer] = 0;
+            //3. Update the round score IF the rolled number was NOT a 1
+            if (dice === 6 && preDice === 6) {
+                scores[activePlayer] = 0;
+                document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+                nextPlayer();
+            } else if (dice !== 1) {
+
+                roundScore += dice;
+                document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+            } else {
+                nextPlayer();
+            }
+            preDice = dice;
+        }
+
+
+    });
+
+    document.querySelector('.btn-hold').addEventListener('click', () => {
+        if (gamePlay) {
+            // Add current score to global score
+            scores[activePlayer] += roundScore;
+
+
+            // Update the UI
             document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-            nextPlayer();
-        } else if (dice !== 1) {
 
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            // Check if player won the game
+            if (scores[activePlayer] >= 1000) {
+                document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+                document.querySelector('.dice').style.display = 'none';
+                document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+                gamePlay = false;
+            } else {
+                nextPlayer();
+            }
 
-        } else {
-            nextPlayer();
-        }
-        preDice = dice;
-    }
-
-
-});
-
-document.querySelector('.btn-hold').addEventListener('click', () => {
-    if (gamePlay) {
-        // Add current score to global score
-        scores[activePlayer] += roundScore;
-
-
-        // Update the UI
-        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-        // Check if player won the game
-        if (scores[activePlayer] >= 1000) {
-            document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-            gamePlay = false;
-        } else {
-            nextPlayer();
         }
 
+    });
+
+    document.querySelector('.btn-new').addEventListener('click', init);
+
+    function init() {
+        scores = [0, 0];
+        activePlayer = 0;
+        roundScore = 0;
+        gamePlay = true;
+        document.querySelector('.dice').style.display = 'none';
+
+        document.getElementById('score-0').textContent = '0';
+        document.getElementById('score-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.getElementById('name-0').textContent = 'Player 1';
+        document.getElementById('name-1').textContent = 'Player 2';
+        document.querySelector('.player-0-panel').classList.remove('winner');
+        document.querySelector('.player-1-panel').classList.remove('winner');
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.remove('active');
+        document.querySelector('.player-0-panel').classList.add('active');
     }
-
-});
-
-document.querySelector('.btn-new').addEventListener('click', init);
-
-function init() {
-    scores = [0, 0];
-    activePlayer = 0;
-    roundScore = 0;
-    gamePlay = true;
-    document.querySelector('.dice').style.display = 'none';
-
-    document.getElementById('score-0').textContent = '0';
-    document.getElementById('score-1').textContent = '0';
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
-    document.querySelector('.player-0-panel').classList.remove('winner');
-    document.querySelector('.player-1-panel').classList.remove('winner');
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.add('active');
 }
+pigGame();
