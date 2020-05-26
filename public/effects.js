e12d77ce0949eee27400cf1773fa938861782718
Argin Simonian <arginsimon@gmail.com>
2020-05-25 20:01:23 -0700

    cube animation done

--- public/effects.js
+++ public/effects.js
@@ -1,4 +1,7 @@
 var stickyNav, threeD, pigGame;
+
+
+
 /* Sticky Navbar Function
  *******************************/
 stickyNav = () => {
@@ -19,15 +22,17 @@ stickyNav();
 /* Introduction Three.js Animation
  ****************************************/
 threeD = () => {
-    var scene, camera, renderer, container, geometry, material, cube, render;
+    var scene, camera, renderer, container, geometry, material, cube, light;
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerWidth, 0.1, 1000);
 
+    camera.position.z = 5;
+
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor("#e5e5e5");
-    container = document.getElementById('intro');
+    renderer.setClearColor("#ff7f50");
     renderer.setSize(window.innerWidth, window.innerHeight);
+    container = document.getElementById('intro');
     container.appendChild(renderer.domElement);
 
     window.addEventListener('resize', () => {
@@ -36,20 +41,60 @@ threeD = () => {
         camera.updateProjectionMatrix();
     })
 
+
+
     geometry = new THREE.BoxGeometry(1, 1, 1);
-    material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+    material = new THREE.MeshLambertMaterial({ color: 0x25b8b8 });
     cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 5;
 
-    render = () => {
+
+
+
+
+    light = new THREE.PointLight(0xFFFFFF, 1, 500)
+    light.position.set(10, 0, 25);
+    scene.add(light);
+
+    var render = () => {
         requestAnimationFrame(render);
+
+
         renderer.render(scene, camera);
     }
-    render();
-}
-threeD();
 
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+
+    function onMouseMove(event) {
+        event.preventDefault();
+
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        raycaster.setFromCamera(mouse, camera);
+
+        var intersects = raycaster.intersectObjects(scene.children, true);
+        for (var i = 0; i < intersects.length; i++) {
+
+
+            this.tl = new TimelineMax();
+            this.tl.to(intersects[i].object.scale, 1, { x: 2, ease: Expo.easeOut })
+            this.tl.to(intersects[i].object.scale, .5, { x: .5, ease: Expo.easeOut })
+            this.tl.to(intersects[i].object.position, .5, { x: 2, ease: Expo.easeOut })
+            this.tl.to(intersects[i].object.rotation, .5, { y: Math.PI * .5, ease: Expo.easeOut }, "=-1.5")
+        }
+    }
+
+    render();
+
+
+
+    window.addEventListener('click', onMouseMove);
+
+}
+
+threeD();
 
 
 /* Pig Game Code
